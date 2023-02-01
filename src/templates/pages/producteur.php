b2c3da54b163b0b148a2ea35ed79bcc5037aa7ae
Floria <florian.richet2004@gmail.com>
2023-02-01 10:30:37 +0100

    Ping placé via formulaire

---
+++ src/templates/pages/producteur.php
@@ -0,0 +1,49 @@
+
+    
+
+<?php
+
+$page_title =" Producteur - CTCBD.com";
+
+ob_start()
+?>
+<div id="accueil_body1">
+<div id="accueil_title">
+        <h1> Cherche Ton CBD | Producteur </h1>
+    </div>
+</div>
+
+<div id="form_producteur">
+<h4 id="form_title">Créer votre établissement</h4>
+        <form method="POST"  action ="" class="form" id="form">
+            <input type="text" id="name" name="name" placeholder="Nom de l'établissement">
+            <br><br> 
+            <input type="text" id="coordlat" name="coord" placeholder="Coordonnée latitude de l'établissement"></textarea><br><br>
+            <input type="text" id="coordlng" name="coord" placeholder="Coordonnée longitude de l'établissement"></textarea><br><br> 
+            <button id="bouton_connexion"> Envoyer </button> <!-- Bouton Envoyer -->
+        </form>
+</div>
+
+<script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+     crossorigin=""></script>
+
+<script type="module" src="../../assets/js/producteur/producteur.js"></script>
+
+<div id="map"></div>
+
+
+
+<?php
+$page_content = ob_get_clean();
+
+
+
+?>
+
+
+
+
+</body>
+</html>
+
---
+++ src/templates/template.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pagecss?>/header_<?=$pagecss?>.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pagecss?>/<?=$page?>.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pagecss?>/footer_<?=$pagecss?>.css'>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+     crossorigin=""/>
+    <title> <?= $page_title;  ?></title>
+</head>
+
+<body>
+    <?= $page_content; ?>
+</body>
+
+</html>
