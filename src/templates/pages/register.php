3bcadda7b12ed55d01696343fb68b266ba475974
Bouard Bastien <bastien.bouard27@gmail.com>
2023-02-01 10:19:14 +0100

    login/register sans chemin css

---
+++ src/templates/pages/register.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <title>Register</title>
+</head>
+<body>
+
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <!---<title> Responsive Registration Form | CodingLab </title>--->
+    <link rel="stylesheet" href="../../www/assets/css/login_register/styleregister.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+
+  <div class="container">
+    <div class="title">Inscription</div>
+    <div class="content">
+      <form action="#">
+        <div class="user-details">
+          <div class="input-box">
+            <span class="details">Nom</span>
+            <input type="text" placeholder="Entrer votre nom" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Prénom</span>
+            <input type="text" placeholder="Entrer votre prénom" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="text" placeholder="Entrer votre email" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Numéro de téléphone</span>
+            <input type="text" placeholder="Entrer votre numéro de téléphone" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Mot de passe</span>
+            <input type="text" placeholder="Entrer votre mot de passe" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Confirmer le mot de passe</span>
+            <input type="text" placeholder="Confirmer votre mot de passe" required>
+          </div>
+        </div>
+        <div class="gender-details">
+          <input type="radio" name="gender" id="dot-1">
+          <input type="radio" name="gender" id="dot-2">
+          <input type="radio" name="gender" id="dot-3">
+          <span class="gender-title">Genre</span>
+          <div class="category">
+            <label for="dot-1">
+            <span class="dot one"></span>
+            <span class="gender">Homme</span>
+          </label>
+          <label for="dot-2">
+            <span class="dot two"></span>
+            <span class="gender">Femme</span>
+          </label>
+          <label for="dot-3">
+            <span class="dot three"></span>
+            <span class="gender">Tracteur</span>
+            </label>
+          </div>
+        </div>
+        <div class="button">
+          <input type="submit" value="Register">
+        </div>
+      </form>
+    </div>
+  </div>
+
+</body>
+</html>
+
+
+
+
+</body>
+</html>
+
+
+
