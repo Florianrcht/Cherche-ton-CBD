37a9189c64fa3f8e9c5a584edae19cf8d86ce7ed
Floria <florian.richet2004@gmail.com>
2023-03-16 11:57:28 +0100

    Clean

--- src/templates/template.php
+++ src/templates/template.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pagecss?>/header_<?=$pagecss?>.css'>
-    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pagecss?>/<?=$page?>.css'>
-    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pagecss?>/footer_<?=$pagecss?>.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pageglobal?>/header_<?=$pageglobal?>.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$page?>/<?=$page?>.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../assets/css/<?=$pageglobal?>/footer_<?=$pageglobal?>.css'>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
      crossorigin=""/>
