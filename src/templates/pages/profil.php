44f9439ef0255c5bb33b847e598ca2defc043bbd
Floria <florian.richet2004@gmail.com>
2023-04-03 15:03:15 +0200

    V3-Header qui change en fonction du statut et page profil

---
+++ src/templates/pages/profil.php
@@ -0,0 +1,40 @@
+<?php
+
+$page_title =" Accueil - CTCBD.com";
+
+ob_start()
+?>
+
+<br><br>
+<br>
+<br>
+<br>
+<br>
+<h3>Bienvenu sur votre profil</h3>
+<br><br>
+<br>
+<br>
+<br>
+<br><br><br>
+<a href="/?page=disconnect" type="button" id="disconnect" >SE DECONNECTER</a>
+
+<br>
+<br>
+<br>
+<br><br><br>
+<br>
+<br>
+<br>
+<br><br><br>
+<br>
+<br>
+<br>
+<br><br><br>
+<br>
+<br>
+<br>
+<br>
+<?php
+$page_content = ob_get_clean();
+
+?>
